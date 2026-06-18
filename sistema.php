ff7c16e1d2a3f8f10d1ec38d268e17af1c9cfb27
ernestojriveror <ernestojriveror1@gmail.com>
2026-06-17 22:07:53 -0300

    Add files via upload

--- sistema.php
+++ sistema.php
@@ -11,11 +11,11 @@
     <?php
     define("NOME_DA_LOJA", "Mi Loja");
     $usuario_cadastrado = "admin";
-    $senha_correta = 1234;
-    $tentativa_usuario = 3;
-    $tentativa_senha = 2;
-    $acesso_concedido = 0;
-    var_dump()
+    $senha_correta = "1234";
+    $tentativa_usuario = "admin";
+    $tentativa_senha = "1234";
+    $acesso_concedido = ($tentativa_usuario === $usuario_cadastrado) && ($tentativa_senha === $senha_correta);
+    var_dump($acesso_concedido);
 
     ?>
 </body>
