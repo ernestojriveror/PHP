1276e4f944e9247c6d40e2cdf92480108f4b1ce5
ernestojriveror <ernestojriveror1@gmail.com>
2026-06-17 22:05:50 -0300

    Add files via upload

---
+++ sistema.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    define("NOME_DA_LOJA", "Mi Loja");
+    $usuario_cadastrado = "admin";
+    $senha_correta = 1234;
+    $tentativa_usuario = 3;
+    $tentativa_senha = 2;
+    $acesso_concedido = 0;
+    var_dump()
+
+    ?>
+</body>
+
+</html>
